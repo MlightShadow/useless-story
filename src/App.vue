ca38b621b760d82ff745fba4d9fcfa074b708ce7
mlightshadow <rwayxu@hotmail.com>
2021-03-13 23:35:23 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!--资源栏位-->
+
+    <field msg="Welcome to Your Vue.js App"/>
+    <!-- 选择 -->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import field from './components/farm.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    field
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import vant from "vant";
+import "vant/lib/index.css";
 
-Vue.config.productionTip = false
+Vue.use(vant);
+
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+    render: (h) => h(App),
+}).$mount("#app");
